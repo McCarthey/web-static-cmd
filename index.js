237e9c19bdcf7f39b83660db4d0afb5dab366263
Dean <mccarthey123@outlook.com>
2018-05-23 10:35:57 +0800

    first edition

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const express = require("express")
+const path = process.cwd()
+
+function startServer(argv) {
+    if (argv[0] === "-h" || argv[0] === "--help") {
+        console.log("usage:\n")
+        console.log("-v or --version [show version]")
+        console.log("-s or --start [start a static server]")
+    } else if (argv[0] === "-v" || argv[0] === "--version") {
+        console.log("v1.0.0")
+    } else if (argv[0] === "-s" || argv[0] === "start") {
+        const app = express()
+        app.use("/", express.static(path)) // ? 想要配置第二个参数
+        app.listen(8088, () => {
+            console.log(`A static server is running on http://localhost:8088`)
+        })
+    }
+}
+
+startServer(process.argv.slice(2))
