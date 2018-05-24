c12213a7dff160e5ce0ff2349408c3bb84ad0bcf
Dean <mccarthey123@outlook.com>
2018-05-24 15:00:34 +0800

    加入proxy 并设置代理参数

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ const proxy = require("http-proxy-middleware")
 const curPath = process.cwd()
 
 function startServer(argv) {
+    console.log(argv)
     if (argv[0] === "-h" || argv[0] === "--help") {
         console.log("usage:\n")
         console.log("-v or --version [show version]")
         console.log("-s or --start [start a static server on current dir]")
-        console.log("-s -p <path> <proxy url> [start a static server on current dir and set a proxy, such as 'sss -s -p /api http://www.example.com']")
+        console.log(
+            "-s -p <path> <proxy url> [start a static server on current dir and set a proxy, such as 'sss -s -p /api http://www.example.com']"
+        )
     } else if (argv[0] === "-v" || argv[0] === "--version") {
-        console.log("v1.0.2")
+        console.log("v1.0.4")
     } else if (argv[0] === "-s" || argv[0] === "start") {
         const app = express()
         app.use("/", express.static(curPath)) // ? 想要配置更多参数
-
         // 代理配置（可选）
         if (argv[1] === "-p" || argv[1] === "--proxy") {
             if (argv[2] && argv[3]) {
+                let pathRewrite = `^/${argv[2]}`
                 app.use(
-                    argv[2],
-                    proxy({
+                    proxy(argv[2], {
                         target: argv[3],
-                        changeOrigin: true
+                        changeOrigin: true,
+                        pathRewrite: {
+                            ["^" + argv[4]]: argv[5]
+                        }
                     })
                 )
             } else {
